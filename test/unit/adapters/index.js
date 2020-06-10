e6424a3cc4b0b3441def01b6149a99a42acde604
Mark Smith <markjgsmith@gmail.com>
2020-06-11 00:45:47 +0700

    Group tests using require in test index files

---
+++ test/unit/adapters/index.js
@@ -0,0 +1,7 @@
+/* eslint-env mocha */
+/* eslint-disable no-unused-expressions */
+'use strict';
+
+describe('adapters', () => {
+  require('./db.test.js');
+});
---
+++ test/unit/functions/index.js
@@ -0,0 +1,8 @@
+/* eslint-env mocha */
+/* eslint-disable no-unused-expressions */
+'use strict';
+
+describe('functions', () => {
+  require('./books.test.js');
+  require('./hello.test.js');
+});
