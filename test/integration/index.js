838ff7ecccff81f407928798e01ed8e3d5e6f670
Mark Smith <markjgsmith@gmail.com>
2020-06-17 03:12:29 +0700

    Add deleteBook route integration tests

--- test/integration/index.js
+++ test/integration/index.js
@@ -4,4 +4,5 @@
 
 describe('integration', () => {
   require('./createBook');
+  require('./deleteBook');
 });
--- test/utils/schemas.js
+++ test/utils/schemas.js
@@ -3,3 +3,8 @@ const Joi = require('joi');
 module.exports.invalidRequestBodySchema = Joi.object().keys({
   message: Joi.string().valid('Invalid request body').required()
 });
+
+module.exports.notFoundSchema = Joi.object().keys({
+  status: Joi.number().valid(404).required(),
+  message: Joi.string().valid('Not found').required()
+});
